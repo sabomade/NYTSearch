8685dccb5f6a2936f4115d88c2c78de657735719
Sara Bolduc <sara.s.bolduc@gmail.com>
2019-10-31 21:29:06 -0700

    updates app.js and index.html

--- javascript/app.js
+++ javascript/app.js
@@ -1,12 +1,18 @@
 var NYTapi = "Tykasc2oGy89sdF1XiOBQPZNgVbSvzAG";
+var searchTerm = $(".search-term").attr()
 
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+searchTerm+"&api-key="+NYTapi;
 
-var searchTerm = "Dogs";
+
 
 $.ajax({
     url: queryURL,
     method: "GET"
-}).then(function(response){
-    console.log(response);
+}).then(function(data){
+    //data.response.docs
+    console.log(data);
+
+    //data.response.docs - for all articles returned
+    var result = data.response.docs;    
+    console.log(result);
 });
